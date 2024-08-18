be8e65105fce766f1e079c7a8430507285f00df2
Samuel <sassani134@gmail.com>
2024-08-18 16:19:48 +0200

    Improve move_res

--- Scripts/move_res.gd
+++ Scripts/move_res.gd
@@ -8,16 +8,19 @@ class_name move extends Resource
 
 @export_category("stats")
 @export var power: int
-@export var acc: int
-@export var pp: int
-
+@export var acc: int = 100
+@export var pp: int = 10
+@export_category("range stats")
 @export var range_min: int = 1
 @export var range_max: int
 @export var zone_min: int = 1
 @export var zone_max: int = 1
-
+@export var canChangeRange: bool = true
+@export var lineZone: bool = false
 @export_category("Target")
 @export var target_self: bool = false
 @export var target_ally: bool = false
 @export var target_foe: bool = true
 @export var target_empty: bool = false
+@export_category("Other")
+@export var critChance: int = 1
