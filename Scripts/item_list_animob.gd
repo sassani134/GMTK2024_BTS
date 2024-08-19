18e5448132432ce0af4b785e7be3d8f2f5f767dd
Samuel <sassani134@gmail.com>
2024-08-19 11:12:05 +0200

    Create a List item Scene retrive object from a dir

---
+++ Scripts/item_list_animob.gd
@@ -0,0 +1,32 @@
+#@tool
+extends ItemList
+
+#"res://Resources/aniMobs/"
+func get_all_file_paths(path: String) -> Array[String]:  
+	var file_paths: Array[String] = []  
+	var dir = DirAccess.open(path)  
+	dir.list_dir_begin()  
+	var file_name = dir.get_next()  
+	while file_name != "":  
+		var file_path = path + "/" + file_name  
+		if dir.current_is_dir():  
+			file_paths += get_all_file_paths(file_path)  
+		else:  
+			file_paths.append(file_path)  
+		file_name = dir.get_next()  
+	return file_paths
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var animobs : Array[String] = get_all_file_paths("res://Resources/aniMobs/")
+	print_debug(animobs)
+	
+	for a in animobs:
+		add_item(a)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
