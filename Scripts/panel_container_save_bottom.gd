10b5876297e928bd2a010d8d19a3679ccf548a66
Samuel <sassani134@gmail.com>
2024-09-09 20:48:40 +0200

    Add some signals

--- Scripts/panel_container_save_bottom.gd
+++ Scripts/panel_container_save_bottom.gd
@@ -1,5 +1,7 @@
 extends PanelContainer
 
+signal save_file
+signal load_file
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,12 +13,15 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_save_pressed() -> void:
-	pass # Replace with function body.
-
 func _on_button_return_pressed() -> void:
 	SceneSwitcher.switch_scene("res://Scenes/start_scene.tscn")
 
 
 func _on_button_load_pressed() -> void:
-	pass # Replace with function body.
+	emit_signal("load_file")
+	print("from pannel")
+
+
+func _on_button_save_pressed() -> void:
+	emit_signal("save_file")
+	print("from pannel")
--- Scripts/team_build_scene.gd
+++ Scripts/team_build_scene.gd
@@ -1,8 +1,12 @@
 extends Control
 
+var save_path: String = "user://teamFiles"
+var save_file_name: String
+var full_path : String = save_path + "/" + save_file_name 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
@@ -18,3 +22,13 @@ func _on_item_list_character_item_selected(index: int) -> void:
 
 func _on_item_list_animob_item_selected(index: int) -> void:
 	print("animob: ",index)
+
+
+func _on_panel_container_save_bottom_load_file() -> void:
+	print("ayo 1")
+	pass # Replace with function body.
+
+
+func _on_panel_container_save_bottom_save_file() -> void:
+	print("ayo 2")
+	pass # Replace with function body.
